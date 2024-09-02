ea973063689f7c0ccdcfd49d3efa66b6bf8ae8ca
Anindyadeep <proanindyadeep@gmail.com>
2024-09-03 02:22:07 +0530

    Added bird dataset using the base class

---
+++ text2sql/datasets/real/bird.py
@@ -0,0 +1,55 @@
+from pathlib import Path
+from typing import Optional, Union
+
+from huggingface_hub import snapshot_download
+from text2sql.datasets.base import Text2SQLBaseDataset
+from text2sql.logger import setup_console_logger
+
+logger = setup_console_logger("[BIRD-DATASET]")
+
+class BirdDataset(Text2SQLBaseDataset):
+    def __init__(
+        self,
+        split: str,
+        dataset_folder: Optional[Union[str, Path]] = "./data",
+        hf_token: Optional[str] = None,
+    ):
+        dataset_folder = Path(dataset_folder)
+        bird_folder = dataset_folder / "bird"
+        if not bird_folder.exists():
+            bird_folder.mkdir(parents=True, exist_ok=True)
+
+            # Download it from hf hub
+            snapshot_download(
+                repo_id="premai-io/birdbench",
+                repo_type="dataset",
+                local_dir=dataset_folder / "bird",
+            )
+
+        dataset_path = bird_folder / split
+        database_folder_name = (
+            "train_databases" if split == "train" else "dev_databases"
+        )
+        json_file_name = "train.json" if split == "train" else "validation.json"
+
+        super().__init__(
+            split=split,
+            dataset_path=dataset_path,
+            database_folder_name=database_folder_name,
+            json_file_name=json_file_name,
+            hf_token=hf_token,
+        )
+        logger.info("Loaded Bird Dataset")
+
+    def setup_dataset(
+        self,
+        filter_by: tuple | None = None,
+        num_rows: int | None = None,
+        num_fewshot: int | None = None,
+        model_name_or_path: str | None = None,
+        prompt_template: str | None = ...,
+    ):  
+        logger.info("Setting up Bird Dataset")
+        return super().setup_dataset(
+            filter_by, num_rows, num_fewshot, model_name_or_path, prompt_template
+        )
