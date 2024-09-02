d0a46a2c5144bb7f6f48d490d1491ed04d610216
Anindyadeep <proanindyadeep@gmail.com>
2024-09-03 02:14:38 +0530

    deleted all the previous files and add .gitignore

--- text2sql/eval/executor/__init__.py
+++
@@ -1,7 +0,0 @@
-from text2sql.eval.executor.bird.acc import BirdExecutorAcc
-from text2sql.eval.executor.bird.ves import BirdExecutorVES
-
-__all__ = [
-    "BirdExecutorAcc",
-    "BirdExecutorVES"   
-]
