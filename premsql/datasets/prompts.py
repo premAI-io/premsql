681d39df500e93980dcc85475936dc0c3c7c6687
Anindyadeep <proanindyadeep@gmail.com>
2024-09-03 19:40:30 +0530

    Add error prompt and old prompts

--- premsql/datasets/prompts.py
+++ premsql/datasets/prompts.py
@@ -22,7 +22,6 @@ query.
 # SQL: 
 """
 
-
 OLD_BASE_TEXT2SQL_PROMPT = """
 # Instruction: 
 - You will be given a question and a database schema.
@@ -43,4 +42,21 @@ a single line (string format).
 # Question: {question}
 
 # SQL:
+"""
+
+ERROR_HANDLING_PROMPT = """
+{existing_prompt}
+
+# Generated SQL: {sql}
+
+## Error Message
+
+{error_msg}
+
+Carefully review the original question and error message, then rewrite the SQL query to address the identified issues. 
+Ensure your corrected query uses correct column names, 
+follows proper SQL syntax, and accurately answers the original question 
+without introducing new errors.
+
+# SQL: 
 """
