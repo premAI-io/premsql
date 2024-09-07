cfa3a1a07ed62e5a92f16a4a48720afb7df8ac73
Anindyadeep <proanindyadeep@gmail.com>
2024-09-07 18:05:37 +0000

    add custom collator for text2sql

---
+++ premsql/datasets/collator.py
@@ -0,0 +1,28 @@
+from dataclasses import dataclass
+from typing import Sequence
+
+import torch
+import transformers
+
+
+@dataclass
+class DataCollatorForSupervisedDataset:
+    tokenizer: transformers.PreTrainedTokenizer
+
+    def __call__(self, instances: Sequence[dict]) -> dict[str, torch.Tensor]:
+        input_ids, labels = tuple(
+            [instance[key] for instance in instances] for key in ("input_ids", "labels")
+        )
+        input_ids = torch.nn.utils.rnn.pad_sequence(
+            input_ids,
+            batch_first=True,
+            padding_value=self.tokenizer.pad_token_id,
+        )
+        labels = torch.nn.utils.rnn.pad_sequence(
+            labels, batch_first=True, padding_value=-100
+        )
+        return dict(
+            input_ids=input_ids,
+            labels=labels,
+            attention_mask=input_ids.ne(self.tokenizer.pad_token_id),
+        )
