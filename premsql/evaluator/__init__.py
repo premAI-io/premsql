2be329f7313628d3d7d7040871db4fe4b7d1f480
Anindyadeep <proanindyadeep@gmail.com>
2024-09-09 15:43:01 +0000

    removed executors

--- premsql/evaluator/__init__.py
+++ premsql/evaluator/__init__.py
@@ -1,4 +1,3 @@
 from premsql.evaluator.base import Text2SQLEvaluator
-from premsql.evaluator.from_sqlite import SQLiteExecutor
 
-__all__ = ["Text2SQLEvaluator", "SQLiteExecutor"]
+__all__ = ["Text2SQLEvaluator"]
