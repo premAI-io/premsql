1eabb99d80abc3dd9b89f65409186d5ff2e8c86a
Anindyadeep <proanindyadeep@gmail.com>
2024-09-09 18:47:30 +0000

    add openai in init

--- premsql/generators/__init__.py
+++ premsql/generators/__init__.py
@@ -1,4 +1,5 @@
 from premsql.generators.huggingface import Text2SQLGeneratorHF
 from premsql.generators.premai import Text2SQLGeneratorPremAI
+from premsql.generators.openai import Text2SQLGeneratorOpenAI
 
-__all__ = ["Text2SQLGeneratorHF", "Text2SQLGeneratorPremAI"]
+__all__ = ["Text2SQLGeneratorHF", "Text2SQLGeneratorPremAI", "Text2SQLGeneratorOpenAI"]
