44a6f69ff3f420fa4731de30245df1fb9548627e
Anindyadeep <proanindyadeep@gmail.com>
2024-09-09 15:46:36 +0000

    added important modules in __init__ for generators

---
+++ premsql/generators/__init__.py
@@ -0,0 +1,7 @@
+from premsql.generators.huggingface import Text2SQLGeneratorHF
+from premsql.generators.premai import Text2SQLGeneratorPremAI
+
+__all__ = [
+    "Text2SQLGeneratorHF",
+    "Text2SQLGeneratorPremAI"
+]
