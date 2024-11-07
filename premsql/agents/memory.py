12e3dd15abca797248e349bee316acadf247a6fd
Anindyadeep <proanindyadeep@gmail.com>
2024-11-07 12:17:48 +0530

    changed the files to cache dir

--- premsql/agents/memory.py
+++ premsql/agents/memory.py
@@ -1,7 +1,8 @@
 import os
+import tempfile
 import sqlite3
-from typing import List, Literal, Optional
 from platformdirs import user_cache_dir
+from typing import List, Literal, Optional
 
 from premsql.logger import setup_console_logger
 from premsql.agents.models import ExitWorkerOutput
@@ -10,15 +11,19 @@ from premsql.agents.utils import convert_exit_output_to_agent_output
 logger = setup_console_logger("[PIPELINE-MEMORY]")
 
 
+
 class AgentInteractionMemory:
     def __init__(self, session_name: str, db_path: Optional[str] = None):
         self.session_name = session_name
         self.db_path = db_path or os.path.join(
-            user_cache_dir(), "premsql", "premsql_pipeline_memory.db"
+            os.getcwd(), "premsql", "premsql_pipeline_memory.db"
         )
+
+        logger.info(self.db_path)
         os.makedirs(os.path.dirname(self.db_path), exist_ok=True)
         self.conn = sqlite3.connect(self.db_path)
         self.create_table_if_not_exists()
+
 
     def list_sessions(self) -> List[str]:
         cursor = self.conn.cursor()
